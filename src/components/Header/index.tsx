178f8e91190ef472c2121bcbbdaf1ed18a22dcd6
Duda Santos <83726420+dudasancar@users.noreply.github.com>
2022-02-16 10:56:00 -0300

    scroll header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,16 +4,51 @@ import Logo from "../../assets/Logo.svg";
 import { Container, Content } from "./styles";
 
 const Header: React.FC = () => {
+  function scrollToAgents() {
+    window.scroll({
+      top: 700,
+      behavior: "smooth",
+    });
+  }
+
+  function scrollToMaps() {
+    window.scroll({
+      top: 1880,
+      behavior: "smooth",
+    });
+  }
+
+  function scrollToGuns() {
+    window.scroll({
+      top: 2600,
+      behavior: "smooth",
+    });
+  }
+
+  function scrollToDownload() {
+    window.scroll({
+      top: 3880,
+      behavior: "smooth",
+    });
+  }
+
+  function scrollToSuporte() {
+    window.scroll({
+      top: 4800,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <Container>
       <Content>
         <img src={Logo} />
         <div>
-          <p>Agentes</p>
-          <p>Mapas</p>
-          <p>Arsenal</p>
-          <p>Download</p>
-          <p>Suporte</p>
+          <p onClick={scrollToAgents}>Agentes</p>
+          <p onClick={scrollToMaps}>Mapas</p>
+          <p onClick={scrollToGuns}>Arsenal</p>
+          <p onClick={scrollToDownload}>Download</p>
+          <p onClick={scrollToSuporte}>Suporte</p>
         </div>
         <Button color="secondary" variant="contained">
           Jogue agora
